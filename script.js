6373cf8ef7b84ff6426a29ee212878b1b259dc5b
thiago-ribeiro1 <thiagoribeiroramos_@outlook.com>
2023-08-17 23:54:46 -0300

    Update

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ function calcular() {
     var consumoPlaca = parseInt(document.getElementById("consumoPlaca").value);
     var largura = parseInt(document.getElementById("largura").value);
     var altura = parseInt(document.getElementById("altura").value);
-
+    var tamanhoHorizontalPlaca = parseFloat(document.getElementById("tamanhoHorizontalPlaca").value);
+    var tamanhoVerticalPlaca = parseFloat(document.getElementById("tamanhoVerticalPlaca").value);
+    var pixelsHorizontal = parseInt(document.getElementById("pixelsHorizontal").value); 
+    var pixelsVertical = parseInt(document.getElementById("pixelsVertical").value); 
+    
     var tamanhoPainel = largura * altura;
-    var tamanhoTotalPlacas = tamanhoPainel * 2;
+    var tamanhoTotalPlacas = tamanhoPainel;
     var consumoTotal = tamanhoTotalPlacas * consumoPlaca;
     var pesoTotal = tamanhoTotalPlacas * pesoPlaca;
-    var totalPixelsHorizontal = (largura * 104) * 2;
-    var totalPixelsVertical = altura * 208;
+    var totalPixelsHorizontal, totalPixelsVertical;
+    
+    if (tamanhoHorizontalPlaca === tamanhoVerticalPlaca) {
+        tamanhoTotalPlacas = tamanhoPainel;
+        totalPixelsHorizontal = largura * pixelsHorizontal;
+        totalPixelsVertical = altura * pixelsVertical;
+    } 
+    if (tamanhoHorizontalPlaca === 0.5 && tamanhoVerticalPlaca === 0.5) {
+        tamanhoTotalPlacas = tamanhoPainel * 4;
+        totalPixelsHorizontal = (largura * pixelsHorizontal) * 2;
+        totalPixelsVertical = (altura * pixelsVertical) * 2;
+        consumoTotal = consumoTotal * 4;
+        pesoTotal = pesoTotal * 4;
+    }
+    else if (tamanhoHorizontalPlaca === 0.5 && tamanhoVerticalPlaca === 1) {
+        tamanhoTotalPlacas = tamanhoPainel * 2;
+        totalPixelsHorizontal = (largura * pixelsHorizontal) * 2;
+        totalPixelsVertical = altura * pixelsVertical;
+        consumoTotal = consumoTotal * 2;
+        pesoTotal = pesoTotal * 2;
+    }
 
     document.getElementById("resultadoTamanhoPainel").textContent = "Tamanho do painel: " + tamanhoPainel + "m²";
     document.getElementById("resultadoTamanhoPlacas").textContent = "Quantidade de placas: " + tamanhoTotalPlacas;
